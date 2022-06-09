7065229487db84a916763ce4b26dfcf5338b915c
Sofia <sofiaxrk@gmail.com>
2022-06-09 15:00:05 +0000

    guardando

--- src/main.py
+++ src/main.py
@@ -2,42 +2,195 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+from datetime import timedelta, datetime
+
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
+
+
+from flask_jwt_extended import create_access_token, get_jwt_identity, jwt_required, JWTManager
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
-#from models import Person
+from sqlalchemy import exc
+from werkzeug.security import check_password_hash, generate_password_hash
+from models import db, User, People, PeopleDetail, Planets, PlanetsDetails
+
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DB_CONNECTION_STRING')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_KEY")
+jwt = JWTManager(app)
+
+
 MIGRATE = Migrate(app, db)
 db.init_app(app)
 CORS(app)
 setup_admin(app)
 
-# Handle/serialize errors like a JSON object
+# Handle/to_dict errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
+
 
 # generate sitemap with all your endpoints
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
 
+@app.route('/login', methods=['POST'])
+def login():
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+
+    if email and password:
+        user = User.get_by_email(email)
+
+        if user and check_password_hash(user._password, password) and user._is_active:
+            """ password = User.get_by_password(password) """ #just in case for species we keep it
+            access_token = create_access_token(identity=user.to_dict(), expires_delta=timedelta(days=30))
+            return jsonify({'token': access_token}), 200
+    
+        return jsonify({'error': 'Invalid information'}), 400
+    return jsonify({"msg": "wrong info"})
+
+
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    users = User.get_all()
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    if users:
+        users_list = [users.to_dict() for user in users]
+        return jsonify(users_list), 200
+    
+    return jsonify({'error': 'Users not found'})
 
-    return jsonify(response_body), 200
+
+@app.route('/user', methods=['POST'])
+def create_user():
+    new_email = request.json.get('email', None)
+    new_username = request.json.get('username', None)
+    new_password = request.json.get('password', None)
+
+    if not (new_email and new_username and new_password):
+        return jsonify({'error': 'Missing user'}), 400
+
+    user_created = User(email=new_email, username=new_username, _password=generate_password_hash(new_password, method='pbkdf2:sha256', salt_length=16))
+
+    try:
+        user_created.create()
+    except exc.IntegrityError:
+        return jsonify({'error': 'Fail in creating user'}), 400
+    
+    account = User.get_by_email(new_email)
+    access_token = create_access_token(identity=account.to_dict(), expires_delta=timedelta(days=30))
+    return jsonify({'token': access_token}), 200
+
+                # GET USER BY ID
+
+@app.route('/user/<int:id>', methods=['GET'])
+def get_user_by_id(id):
+    user = User.get_by_id(id)
+
+    if user:
+        return jsonify(user.to_dict()), 200
+
+        return jsonify({'error': 'User is not found'}), 404
+
+@app.route('/user/<int:id>/favourite', methods=['GET'])
+@jwt_required
+def get_fav(id):
+    token_id = get_jwt_identity()
+
+    if token_id == id:
+        favourites = Favourite.get_all()
+        return favourite
+
+    return jsonify({'error': 'You are not authorized'})
+
+
+
+@app.route('/user/<int:id_user>/favourite-people/<int:id_people>', methods=['POST'])
+@jwt_required()
+def create_fav(id_user,id_people):
+    token_id = get_jwt_identity()
+
+    if token_id.get("id") == id_user:
+        user = User.get_by_id(id_user)
+        people = People.get_by_id(id_people)
+
+        if user and people:
+            add_people = user.new_fav_people(people)
+            fav_people = [people.to_dict() for people in add_people]
+            return jsonify(fav_people), 200
+
+    return jsonify({'error': 'No favourites'}), 404
+
+
+@app.route('/planets/', methods=['GET'])
+def get_all_planets():
+    planet_s = Planets.get_all()
+
+    if planet_s:
+        all_planets = [planets.to_dict() for planets in planet_s]
+        return jsonify(all_planets), 200
+
+    return jsonify({'message':'Planets not found'}), 400
+
+
+@app.route('/planets/<int:id>/planetsdetails', methods=['GET'])
+def get_planets(id):
+    all_planets = Planets.get_by_id_planetsdetails(id)
+
+    if all_planets:
+        return jsonify(all_planets.to_dict()), 200
+
+    return jsonify({'message':'Planets not found'}), 400
+
+
+@app.route('/people', methods=['GET'])
+def get_all_people():
+    characters = People.get_all()
+
+    if characters:
+        character_list = [character.serialize() for character in characters] 
+        return jsonify({character_list}), 200
+
+
+    return jsonify({'error': 'Characters not found'}), 400
+
+
+@app.route('/people/<int:id>', methods=['GET'])
+def get_character(id):
+    character = People.get_by_id(id)
+
+    if character:
+        return jsonify(character.serialize()), 200
+
+
+@app.route('/user/<int:id_user>/favourite-planets/<int:id_planets>', methods=['POST'])
+@jwt_required()
+def add_favplanets(id_user,id_planets):
+    token_id = get_jwt_identity()
+    print(token_id)
+
+    if token_id.get("id") == id_user:
+        user = User.get_user_by_id(id_user)
+        planets = Planets.get_by_id(id_planets)
+        
+        if user and planets:
+            add_fav = user.add_fav_planets(planets)
+            fav_planets = [planets.to_dict() for planets in add_fav]
+            return jsonify(fav_planets),200
+
+    return jsonify({'error': 'Not authorized'}),404
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -2,18 +2,210 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+                              
+planetsfavourites = db.Table('planetsfavourites',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('planets_id', db.Integer, db.ForeignKey('planets.id'), primary_key=True))
+
+favourite_character= db.Table('favourite_character',
+    db.Column("user_id", db.Integer, db.ForeignKey("user.id"), primary_key=True),
+    db.Column("people_id", db.Integer, db.ForeignKey("people.id"), primary_key=True))
+
 class User(db.Model):
+    __tablename__: 'user'
+
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    username = db.Column(db.String(120), unique=True, nullable=False)
+    _password = db.Column(db.String(1000), unique=False, nullable=False)
+    _is_active = db.Column(db.Boolean(), unique=False, nullable=False, default=True)
+
+    have_user_planets = db.relationship('Planets', secondary = speciesfavourites, back_populates="have_user_planetsfav")
+
+    have_char = db.relationship("People", secondary=favourite_character, back_populates="have_user")
+                              
+    
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User is {self.username}, with {self.email} and {self.id}'
+    
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "username":self.username,
+            "email": self.email,
+            "planets": [planets.to_dict() for planets in self.have_user_planets],
+            "characters": [people.to_dict() for people in self.have_char] 
+        }
+
+    def create(self):
+       db.session.add(self)
+       db.session.commit()
+
+
+    @classmethod
+    def get_by_email(cls, email):
+        account = cls.query.filter_by(email=email).one_or_none()
+        return account
+
+
+    @classmethod
+    def get_by_password(cls, password):
+        secretPass = cls.query.filter_by(password=password).one_or_none()
+        return secretPass
+
+    @classmethod
+    def get_all(cls):
+        users = cls.query.all()
+        return users
+  
+
+    @classmethod
+    def get_by_id(cls, id):
+        user_id = cls.query.get(id)
+        return user_id
+
+
+
+
+
+@classmethod
+def get_by_id(cls, id):
+    starship = cls.query.get(id)
+    return starship
+                              
+    @classmethod
+    def get_user_by_id(cls,id):
+        user = cls.query.get(id)
+        return user
+    
+    def add_fav_species(self,specie):
+        self.have_user_species.append(specie)
+        db.session.commit()
+        return self.have_user_species
+    
+    def new_fav_people (self,people):
+        self.have_char.append(people)
+        db.session.commit()
+        return self.have_char
+
+
+class Planets(db.Model):
+    __tablename__:"planets"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    planets_id = db.Column(db.Integer, db.ForeignKey("planets_details.id"), nullable=False)
+
+    planets_have = db.relationship("PlanetsDetails", back_populates = "planets_have_details")
+    have_user_speciesfav= db.relationship('User', secondary= speciesfavourites, back_populates="have_user_species")
+
+    def __repr__(self):
+        return f'Planets: {self.id}, name: {self.name}'
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+        }
+        
+    @classmethod    
+    def get_all(cls):
+        planets = cls.query.all()
+        return planets
+
+    @classmethod
+    def get_by_id(cls, id):
+        planets = cls.query.get(id)
+        return planets
+                              
+class People(db.Model):
+    __tablename__: "people"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    detail_id = db.Column(db.Integer, db.ForeignKey("people_detail.id"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    people_has_details = db.relationship("PeopleDetail", back_populates="detail_has_character")
+    have_user = db.relationship("User", secondary=favourite_character, back_populates="have_char")
+
+
+    def __repr__(self):
+        return f'People is {self.name}, url: {self.url}'
 
     def serialize(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name
+        }
+
+    @classmethod
+    def get_all(cls):
+        character_list = cls.query.all()
+        return character_list
+
+
+    @classmethod
+    def get_by_id(cls, id):
+        character = cls.query.get(id)
+        return character
+       
+                        
+class PlanetsDetails(db.Model):
+    __tablename__:"planets_details"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+
+    planets_have_details = db.relationship('Planets', back_populates="planets_have")
+
+    def __repr__(self):
+        return f'PlanetsDetails is {self.classificacion}, name:{self.name}' 
+        
+    def to_dict(self):
+        return {
+            "id" : self.id,
+            "name": self.name,
+        }
+    
+    @classmethod
+    def get_all_planetsdetails(cls):
+        planetsdetails = cls.query.all()
+        return planetsdetails
+    
+    @classmethod
+    def get_by_id_planetsdetails(cls,id):
+        planetsdetails = cls.query.get(id)
+        return planetsdetails
+   
+
+class PeopleDetail(db.Model):
+    __tablename__: "people_detail" 
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=False, nullable=False)
+    eye_color = db.Column(db.String(250), unique=False, nullable=False)
+    gender = db.Column(db.String(250), unique=False, nullable=False)
+    homeworld = db.Column(db.String(250), unique=False, nullable=False)
+
+    detail_has_character = db.relationship("People", back_populates="people_has_details")
+
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "height": self.height,
+            "mass": self.mass,
+            "hair_color": self.hair_color,
+            "skin_color": self.skin_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+            "gender": self.gender,
+            "created": self.created,
+            "edited": self.edited,
+            "homeworld": self.homeworld
         }
